62df3a4d7ab6a5d3e52696205c3bd1410de84a3b
Chris Irwin <pianowow@gmail.com>
2025-11-13 20:35:54 -0500

    npm build

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/rocket_math_web/',
   plugins: [
     vue(),
     vueDevTools(),
